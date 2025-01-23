8ed53e28b6cdd4b98bc4dc18e08f1e851b44bd1c
Leonardo Salvarani <S2402984@ndonline.ac.uk>
2025-01-23 09:23:17 +0000

    Finished Question 2

--- Pandas Lessons/Questions/Q02_Summarising Data.py
+++ Pandas Lessons/Questions/Q02_Summarising Data.py
@@ -19,6 +19,8 @@ print("look at the first 25 entries")
 
 #Your code goes here:
 
+print(chipo.head(25))
+
 print("#"*10)
 input()
 
@@ -27,13 +29,17 @@ print("find out how many rows are in the dataset")
 
 #Your code goes here:
 
+print(chipo.shape[0])
+
 print("#"*10)
 input()
 
 print("step 6")
-print("find out how many columns are in the datasety")
+print("find out how many columns are in the dataset")
 
 #Your code goes here:
+
+print(chipo.shape[1])
 
 print("#"*10)
 input()
@@ -43,6 +49,8 @@ print("find out the names of all the columns")
 
 #Your code goes here:
 
+print(chipo.columns)
+
 print("#"*10)
 input()
 
@@ -50,6 +58,8 @@ print("step 8")
 print("find out how the dataset is indexed")
 
 #Your code goes here:
+
+print(chipo.index)
 
 print("#"*10)
 input()
@@ -59,6 +69,8 @@ print("describe the dataset, make sure to include every column")
 
 #Your code goes here:
 
+print(chipo.describe(include="all"))
+
 print("#"*10)
 input()
 
@@ -66,6 +78,8 @@ print("step 10")
 print("find the most ordered item")
 
 #Your code goes here:
+
+print(chipo['item_name'].value_counts().head(1))
 
 print("#"*10)
 input()
@@ -75,6 +89,8 @@ print("find out how many times the item from step 10 was ordered")
 
 #Your code goes here:
 
+print(chipo['item_name'].value_counts().max())
+
 print("#"*10)
 input()
 
@@ -82,6 +98,8 @@ print("step 12")
 print("find out the most ordered item in the choice_description colum")
 
 #Your code goes here:
+
+print(chipo['choice_description'].value_counts().head(1))
 
 print("#"*10)
 input()
@@ -91,6 +109,8 @@ print("find out how many items were ordered in total")
 
 #Your code goes here:
 
+print(chipo['quantity'].sum())
+
 print("#"*10)
 input()
 
@@ -99,6 +119,8 @@ print("find out how many orders are in the entire dataset")
 
 #Your code goes here:
 
+print(chipo['order_id'].nunique())
+
 print("#"*10)
 input()
 
@@ -106,6 +128,8 @@ print("step 15")
 print("find out how many different items are sold in this dataset")
 
 #Your code goes here:
+
+print(chipo['item_name'].nunique())
 
 print("#"*10)
 input()
@@ -116,6 +140,8 @@ print("find out the data type of the item_price column")
 
 #Your code goes here:
 
+print(chipo['item_price'].dtype)
+
 print("#"*10)
 input()
 
@@ -123,6 +149,8 @@ print("step 17")
 print("change the data type of the item_price column to float")
 
 #Your code goes here:
+
+chipo['item_price'] = chipo['item_price'].str.replace('$', '').astype(float)
 
 print("#"*10)
 input()
@@ -132,6 +160,8 @@ print("find out the total revenue of all orders in the dataset")
 
 #Your code goes here:
 
+print(chipo['item_price'].sum())
+
 print("#"*10)
 input()
 
@@ -139,6 +169,8 @@ print("step 19")
 print("find out the average price of an order")
 
 #Your code goes here:
+
+print(chipo.groupby('order_id')['item_price'].sum().mean())
 
 print("#"*10)
 input()
