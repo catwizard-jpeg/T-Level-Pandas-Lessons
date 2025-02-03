0bc6f4b970a67640f92038811a06119152668965
sdlevynd <136323565+sdlevynd@users.noreply.github.com>
2025-02-03 15:10:37 +0000

    Update Task02_Flight Search.py

--- Pandas Lessons/Questions/Task02_Flight Search.py
+++ Pandas Lessons/Questions/Task02_Flight Search.py
@@ -4,3 +4,10 @@ import matplotlib.pyplot as plt
 df = pd.read_csv("Data/flights.csv")
 
 print(df)
+
+#program brief
+#let the user select a date
+#let the user select a flight path
+#let the user select between AM and PM
+#show the user the flights running at the time, on that path
+#repeat
